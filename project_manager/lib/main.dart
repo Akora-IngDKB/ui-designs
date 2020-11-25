900081af0508a915b9f504752c609c749127e158
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-11-25 22:24:46 +0000

    PM: Initial commit

---
+++ project_manager/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project_manager/src/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Homepage(),
+    );
+  }
+}
