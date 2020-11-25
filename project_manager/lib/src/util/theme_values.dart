ddb1122cfcedd1b5186cc881fb770bf2b85f9f5c
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-11-25 23:52:53 +0000

    PM: Added some assets

---
+++ project_manager/lib/src/util/theme_values.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const LIGHT_ORANGE = Color(0xFFFFF9EC);
+const ORANGE = Color(0xFFF9BE7C);
+const RED_ORANGE = Color(0xFFE46472);
+const LIGHT_BLUE = Color(0xFF6488E4);
+const GREEN = Color(0xFF309397);
+const BLACK = Color(0xFF0D253F);
